48eae5774226c4485ac03533bd99327bd3ac8069
PedrooCamilo <pedro2005camilo@gmail.com>
2025-05-05 19:02:26 -0300

    add pdf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,20 @@
-// src/app.controller.ts
-import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
-import { join } from 'path';
-import { createReadStream } from 'fs';
+  import { Controller, Get, Res } from '@nestjs/common';
+  import { Response } from 'express';
+  import { join } from 'path';
+  import { createReadStream } from 'fs';
 
-@Controller()
-export class AppController {
-  @Get('download')
-  downloadPDF(@Res() res: Response) {
-    const filePath = join(__dirname, '..', 'pdfs', 'arquivo.pdf');
-    const fileStream = createReadStream(filePath);
+  @Controller()
+  export class AppController {
+    @Get('download')
+    downloadPDF(@Res() res: Response) {
+      const filePath = join(__dirname, '..', 'pdfs', 'arquivo.pdf');
+      const fileStream = createReadStream(filePath);
 
-    res.set({
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename=arquivo.pdf',
-    });
+      res.set({
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'attachment; filename=arquivo.pdf',
+      });
 
-    fileStream.pipe(res);
+      fileStream.pipe(res);
+    }
   }
-}
